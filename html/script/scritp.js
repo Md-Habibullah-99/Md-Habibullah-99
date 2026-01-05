f41e9e8f410257dd25ed09ee6724b208e7f2ce45
md-habibullah-99 <habibullahhisam@gmail.com>
2026-01-05 23:44:39 +0600

    Remove all files to add a readme to the profile.

--- html/script/scritp.js
+++
@@ -1,7 +0,0 @@
-let a,b;
-function output(){
-  a = Number(document.querySelector(".input1").value);
-  b = Number(document.querySelector(".input2").value);
-  html = `The sum is : ${a+b}`;
-  document.querySelector(".output").innerHTML = html;
-};
